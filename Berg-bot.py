8524ff9b9f6ea996bd4f8cc276ad1b70b15d05ea
Yuri <YJ14126P@pace.edu>
2024-12-03 13:44:57 -0500

    yes

--- Berg-bot.py
+++ Berg-bot.py
@@ -1,1 +1,1 @@
-info_tracker = {'First name': 0,"Last Name": 1, "Birth Date": 2, "Email": 3, "Address": 4, "Entry Term": 5, "Major": 6, "Sex": 7}
+info_track = {'First name': 0,"Last Name": 1, "Birth Date": 2, "Email": 3, "Address": 4, "Entry Term": 5, "Major": 6, "Sex": 7}
