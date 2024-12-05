23ef40c00941173ef194b51e9d7b30e1be5dab67
Yuri <YJ14126P@pace.edu>
2024-12-05 12:00:44 -0500

    qna

--- Berg-bot.py
+++ Berg-bot.py
@@ -1,1 +1,5 @@
-info_track = {'First name': 0,"Last Name": 1, "Birth Date": 2, "Email": 3, "Address": 4, "Entry Term": 5, "Major": 6, "Sex": 7}
+info_track = {'First name': 0,"Last Name": 1, "Birth Date": 2, "Email": 3, "Address": 4, "Entry Term": 5, "Major": 6, "GPA": 7}
+
+for k,v in info_track.items():
+    print(f'enter {k} code{v}')
+    
