6d1b419e699f301991ecf8fd2068fc6c6d21ecd8
Yuri <YJ14126P@pace.edu>
2024-12-10 13:48:52 -0500

    res update

--- Berg-bot.py
+++ Berg-bot.py
@@ -1,5 +1,25 @@
-info_track = {'First name': 0,"Last Name": 1, "Birth Date": 2, "Email": 3, "Address": 4, "Entry Term": 5, "Major": 6, "GPA": 7}
+info_track = {'first_name': 0,"last_name": 1, "birth_date": 2, "email": 3, "address": 4, "entry_term": 5, "major": 6, "gpa": 7}
 
-for k,v in info_track.items():
-    print(f'enter {k} code{v}')
+# for k,v in info_track.items():
+#     print(f'enter {k} code {v}')
+
+accum = 0
+lst = ['why', 'what', 'where']
+lstresp = ['It works for why', 'It works for what', 'It works for where']
+
+while True:
+    email = input("Enter your email. (Enter help if needed) ")
+    if email.lower() == 'help':
+        question = input("What do you need help with? ")
+        print(question.split())
+        print([x for x in question.split() if x in lst])
+        print(lstresp[lst.index(''.join([x for x in question.split() if x in lst]))])
+        
+        #if question.split() in lst:
+
+
+
+
+#for question in range(8):
+    #print(question)
     
